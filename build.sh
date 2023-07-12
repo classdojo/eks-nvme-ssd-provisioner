64175dd0cdf0d78dfa313cba9ef18218d775a68a
Seandon Mooy <seandon.mooy@gmail.com>
2023-07-12 08:38:07 -0700

    upgrade to bullseye, add build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+PLATFORMS="linux/arm64,linux/amd64"
+
+ECR_NAME="classdojo/eks-nvme-ssd-provisioner"
+REPO_TAG="$(git rev-parse --short HEAD)"
+
+aws ecr describe-repositories --repository-names "${ECR_NAME}" --no-paginate --output json --no-cli-pager > /dev/null || {
+    echo "Creating ECR repo: ${ECR_NAME}"
+    aws ecr create-repository \
+        --repository-name "${ECR_NAME}" \
+        --region us-east-1 \
+        --no-paginate --output json --no-cli-pager 
+}
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 347708466071.dkr.ecr.us-east-1.amazonaws.com
+COMPLETE_TAG="347708466071.dkr.ecr.us-east-1.amazonaws.com/${ECR_NAME}:${REPO_TAG}"
+
+docker buildx build \
+    --platform "${PLATFORMS}" \
+    -t "${COMPLETE_TAG}" \
+    --pull \
+    --push .
+
+echo -e "\n========> Image now available at: ${COMPLETE_TAG}"
